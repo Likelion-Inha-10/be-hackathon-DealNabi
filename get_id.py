a1926ba1e11557de5c55fc712dc506e74052111c
hylee <12201299@inha.edu>
2022-05-08 22:09:32 +0900

    1번 기능

---
+++ get_id.py
@@ -0,0 +1,23 @@
+from msilib import sequence
+import requests
+from bs4 import BeautifulSoup
+
+def get_id(n):
+
+    url = "https://www.googleapis.com/youtube/v3/videos"
+
+
+    params = {
+        "key": "AIzaSyC-EdQIhOL5j0VcUeVgZyARJZOt3cBtzd0",
+        "part": "id",
+        "chart": "mostPopular",
+        "regionCode": "KR",
+        "maxResults": n
+    }
+
+    response = requests.get(url, params=params).json()['items']
+    video_List=[]
+    for idx in range(len(response)):
+        video_List.append(response[idx]['id'])
+
+    return(video_List)
